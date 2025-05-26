50a465213087d54ad77df8dc1d6a35a194c38b1d
alq-)a(-ida <forvladwork@gmail.com>
2025-05-26 18:26:48 +0300

    Add MovieDetails+Go Back+ some Profile+some Saved

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,14 +1,42 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
 
 const profile = () => {
   return (
-    <View>
-      <Text>profile</Text>
+    <View style={styles.Profile_container}>
+      <View style={styles.Profile_header_container}>
+        <Image
+          resizeMode="contain"
+          style={styles.Profile_Image}
+          source={require("@/assets/images/logo.png")}
+        ></Image>
+        <Text style={styles.Profile_Text}>Profile</Text>
+      </View>
     </View>
   );
 };
 
 export default profile;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  Profile_container: {
+    backgroundColor: "#01140c",
+    flex: 1,
+  },
+  Profile_header_container: {
+    width: "100%",
+    height: "100%",
+    flexDirection: "column",
+    paddingHorizontal: 30,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  Profile_Image: {
+    width: 100,
+    height: 100,
+  },
+  Profile_Text: {
+    color: "white",
+    fontSize: 25,
+  },
+});
--- app/movies/[id].tsx
+++ app/movies/[id].tsx
@@ -1,15 +1,41 @@
 import { fetchMovieDetails } from "@/servises/api";
 import useFetch from "@/servises/useFetch";
-import { useLocalSearchParams } from "expo-router";
+import { router, useLocalSearchParams } from "expo-router";
 import React from "react";
-import { Image, ScrollView, StyleSheet, Text, View } from "react-native";
+import {
+  Image,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+interface MovieInfoProps {
+  label: string;
+  value: string | number | null;
+}
 const MovieDetails = () => {
   const { id } = useLocalSearchParams();
   const { data: movie, loading } = useFetch(() =>
     fetchMovieDetails(id as string)
   );
+  const MovieInfo = ({ label, value }: MovieInfoProps) => (
+    <View style={styles.MovieInfo_container}>
+      <Text style={styles.MovieInfo_Header_Text}>{label}</Text>
+      <Text style={styles.MovieInfo_Overview}>{value || "N/A"}</Text>
+    </View>
+  );
   return (
     <View style={styles.MovieDetails_container}>
+      <TouchableOpacity onPress={router.back} style={styles.Arrow_Back_Touch}>
+        <View style={styles.Arrow_Back_View}>
+          <Image
+            source={require("@/assets/icons/ArrowBackWhite.png")}
+            style={styles.Arrow_Back}
+          ></Image>
+          <Text style={styles.Arrow_Back_Text}>Go back</Text>
+        </View>
+      </TouchableOpacity>
       <ScrollView
         contentContainerStyle={{
           width: "100%",
@@ -30,13 +56,40 @@ const MovieDetails = () => {
           <View style={styles.ForDetails_text}>
             <Text style={styles.ForDetails_Box2}>{movie?.release_date}</Text>
             <Text style={styles.ForDetails_Box2}>
-              RunTime: {movie?.runtime}
+              RunTime: {movie?.runtime}m
             </Text>
           </View>
+          <View style={styles.ForDetails_Box3}>
+            <Image
+              style={styles.ForDetails_Box3_Icon}
+              source={require("@/assets/icons/Star.png")}
+            ></Image>
+            <Text style={styles.ForDetails_Box3_Text}>
+              {movie?.vote_average ?? 0}
+            </Text>
+            <Text style={styles.ForDetails_Box3_Text}>
+              ({movie?.vote_count} votess)
+            </Text>
+          </View>
+          <MovieInfo label="Overview" value={movie?.overview}></MovieInfo>
+          <MovieInfo
+            label="Genres"
+            value={movie?.genres?.map((g: any) => g.name).join(" - ") || "N/A"}
+          ></MovieInfo>
+          <View style={styles.Budget_container}>
+            <MovieInfo label="Budget" value={`$${movie?.budget}`}></MovieInfo>
+            <MovieInfo label="Revenue" value={`$${movie?.revenue}`}></MovieInfo>
+          </View>
+          <MovieInfo
+            label="Production Companies"
+            value={
+              movie?.production_companies
+                ?.map((pc: any) => pc.name)
+                .join(" - ") || "N/A"
+            }
+          ></MovieInfo>
         </View>
       </ScrollView>
-
-      <Text>MovieDetails:[id]</Text>
     </View>
   );
 };
@@ -55,10 +108,10 @@ const styles = StyleSheet.create({
   },
   ForDetails_container: {
     width: "100%",
-    marginTop: 20,
     flexDirection: "column",
     justifyContent: "flex-start",
     paddingHorizontal: 15,
+    gap: 15,
   },
   ForDetails_title: {
     fontSize: 30,
@@ -76,6 +129,71 @@ const styles = StyleSheet.create({
     padding: 6,
     backgroundColor: "green",
     fontSize: 18,
+    borderRadius: 5,
     fontWeight: "400",
+  },
+  ForDetails_Box3: {
+    flexDirection: "row",
+    gap: 10,
+    alignItems: "center",
+  },
+  ForDetails_Box3_Text: {
+    color: "white",
+    backgroundColor: "green",
+    fontSize: 18,
+    borderRadius: 5,
+    padding: 5,
+    fontWeight: "400",
+  },
+  ForDetails_Box3_Icon: {
+    width: 28,
+    height: 28,
+  },
+  MovieInfo_container: {
+    flexDirection: "column",
+    marginTop: 10,
+    gap: 10,
+  },
+  MovieInfo_Header_Text: {
+    color: "white",
+    fontWeight: "semibold",
+    fontSize: 24,
+  },
+  MovieInfo_Overview: {
+    color: "white",
+    fontSize: 18,
+    fontWeight: "400",
+  },
+  Budget_container: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+  },
+  Arrow_Back_Touch: {
+    width: 0,
+    height: 0,
+    zIndex: 1000,
+    position: "fixed",
+    flexDirection: "column",
+    top: 55,
+    right: "-75%",
+  },
+  Arrow_Back_View: {
+    backgroundColor: "red",
+    borderRadius: 5,
+    width: 80,
+    height: 76,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  Arrow_Back: {
+    width: 50,
+    height: 50,
+    borderRadius: 5,
+  },
+  Arrow_Back_Text: {
+    zIndex: 1001,
+    color: "white",
+    fontSize: 16,
+    fontWeight: "semibold",
   },
 });
